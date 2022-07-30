72ae4c91221df934d7178487e4f3e7c8121225a5
ChaitanyaTabhane <71386171+ChaitanyaTabhane@users.noreply.github.com>
2022-07-31 00:28:08 +0530

    Update ReverseimplementationOfLinkedList.cpp

--- ReverseimplementationOfLinkedList.cpp
+++ ReverseimplementationOfLinkedList.cpp
@@ -21,6 +21,23 @@ void InsertAtHead(Node* &head, int d){
     head = temp;
 }
 
+void InsertAtTail(Node* &tail, int data){
+    Node* temp = new Node(data);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void InsertAtPosition(Node* head, int pos, int data){
+    Node* temp = head;
+    int count = 1;
+    while(count< pos -1){
+        temp = temp -> next;
+        count++;
+    }
+    Node* nodeToInsert = new Node(data);
+    nodeToInsert  = temp -> next;
+    temp -> next = nodeToInsert;
+}
 
 void print(Node* &head){
     Node* temp = head;
@@ -40,9 +57,20 @@ int main() {
     // cout<< node2 -> data << endl;
     // cout<< node2 -> next << endl;
     Node* head = node1;
-    InsertAtHead(head, 30);
-    InsertAtHead(head, 30);
-    InsertAtHead(head, 30);
+    Node* tail = node1;
+    
+    // InsertAtHead(head, 30);
+    // InsertAtHead(head, 30);
+    // InsertAtHead(head, 30);
+    
+    InsertAtTail(tail,6);
+    InsertAtTail(tail,7);
+    InsertAtTail(tail,2);
+    InsertAtTail(tail,3);
+    
+    print(head);
+    
+    InsertAtPosition(head, 3, 3);
     
     print(head);
     return 0;
